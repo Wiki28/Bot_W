4554946120da995e4bd38bdad8dfbd00b29b216d
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-11-22 06:00:48 +0800

    Update nsfw-all.js

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://nzrlafndi.xyz/api/nsfw/$%7Bcommand%7D?apikey=${nzrlkey}`
+let nyenye = `https://NzrlAfndi.xyz/api/nsfw/${command}?apikey=${nzrlkey}`
     conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
