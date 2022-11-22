f055d1d8fd7a3e13f619754b8ec73871e6aa206a
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-11-22 09:06:01 +0800

    Update main-Tesintro.js

--- plugins/main-Tesintro.js
+++ plugins/main-Tesintro.js
@@ -1,18 +1,15 @@
 let handler = async (m, { conn }) => await conn.sendMessage(m.chat, {text: `
-▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 Silahkan isi Data Di Bawah ini 
 untuk memperkenalkan diri kamu
-▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
-🚀  Nama :
-🚀  Umur :
-🚀  askot :
-🚀  spill ig :
-🚀  spill tiktok :
-🚀  Alasan masuk grup :
-▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+Kalau tidak mau di kosong kan saja
+Nama :
+Umur :
+askot :
+spill ig :
+spill tiktok :
+Alasan masuk grup :
 Selamat Bergabung Semoga Betah 
 Patuhi Peraturan Di Group ini
-▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰ 
 `}, { quoted: m})
 handler.customPrefix = /^(intro ngab|intro|member baru|new member|intro kak|intro dek)$/i
 handler.command = new RegExp
